d117c54b3c70d6b91200026ac8e502a91352cb00
94756970+waynemwashuma@users.noreply.github.com <94756970+waynemwashuma@users.noreply.github.com>
2023-12-23 11:28:32 +0300

    Graph drawn successfully

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { Renderer2D, Vector2 } from "./chaos.module.js"
+import { Renderer2D, Vector2, circle, rand } from "./chaos.module.js"
 
 class Path {
   /**
@@ -18,7 +18,9 @@ class Path {
    * @param {Node} node
    */
   constructor(to, from) {
-
+    this.to = to
+    this.from = from
+    this.distance = new Vector2().copy(to.position).sub(from.position).magnitude()
   }
 }
 class Node {
@@ -36,10 +38,18 @@ class Node {
   /**
    * @param {Node} node
    */
-  addPathTo(node) {
-
+  pathTo(node) {
+    let path = new Path(node, this)
+    this.paths.push(path)
   }
-  draw() {
+  /**
+   * @param {CanvasRenderingContext2D} ctx
+   */
+  draw(ctx) {
+
+
+
+    
 
   }
 }
@@ -51,18 +61,68 @@ class Graph {
   constructor() {
 
   }
-  draw() {
-
+  /**
+   * @param {CanvasRenderingContext2D} ctx
+   */
+  draw(ctx) {
+    ctx.beginPath()
+    ctx.lineWidth = 7
+    this.nodes.forEach(e => {
+      e.paths.forEach(p => {
+        ctx.moveTo(...p.from.position)
+        ctx.lineTo(...p.to.position)
+      })
+      ctx.strokeStyle = "green"
+      ctx.stroke()
+    })
+    ctx.closePath()
+    this.nodes.forEach(e => {
+      ctx.beginPath()
+      circle(ctx, ...e.position, 10)
+      ctx.fillStyle = "black"
+      ctx.fill()
+      ctx.closePath()
+    })
+  }
+  /**
+   * @param { Node } node
+   */
+  add(node) {
+    this.nodes.push(node)
   }
 }
 
 let renderer = new Renderer2D()
-let graph= new Graph()
+let graph = new Graph()
 
 renderer.bindTo('#body')
 renderer.setViewport(innerWidth, innerHeight)
 renderer.add({
-  render(){
+  render(ctx) {
     graph.draw(ctx)
   }
 })
+renderer.play()
+
+for (let i = 0; i < 6; i++) {
+  const node = new Node()
+
+  node.position.set(rand(0, renderer.width), rand(0, renderer.height))
+
+  graph.add(node)
+}
+
+for (let i = 0; i < graph.nodes.length; i++) {
+  const node = graph.nodes[i]
+
+  for (let j = 0; j < graph.nodes.length; j++) {
+    const to = graph.nodes[j]
+
+    if (to === node) continue
+
+    node.pathTo(to)
+  }
+
+}
+
+console.log(graph)
