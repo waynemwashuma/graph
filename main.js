4c81c1c2f72499dc42cc2300efca9bfd2293a75e
94756970+waynemwashuma@users.noreply.github.com <94756970+waynemwashuma@users.noreply.github.com>
2024-01-02 18:35:36 +0300

    Refactored code to use a heuristic function

--- main.js
+++ main.js
@@ -12,17 +12,12 @@ class Path {
    */
   from = null
   /**
-   * @type { number } 
-   */
-  distance = 0
-  /**
    * @param {Node} node
    * @param {Node} node
    */
   constructor(to, from) {
     this.to = to
     this.from = from
-    this.distance = new Vector2().copy(to.position).sub(from.position).magnitude()
   }
 }
 class Node {
@@ -56,6 +51,9 @@ class Node {
       if (this.paths[k].to === node) return true
     }
     return false
+  }
+  removePathTo(node,removed = false){
+    
   }
 }
 class Graph {
@@ -101,54 +99,20 @@ let graph = new Graph()
 
 renderer.bindTo('#body')
 renderer.setViewport(innerWidth, innerHeight)
-
-renderer.play()
-
-for (let i = 0; i < 10; i++) {
-  const node = new Node()
-
-  node.position.set(
-    rand(0, renderer.width),
-    rand(0, renderer.height)
-  )
-
-  graph.add(node)
-}
-
-for (let i = 0; i < graph.nodes.length; i++) {
-  const node = graph.nodes[i]
-
-  for (let j = 0; j < graph.nodes.length; j++) {
-    const to = graph.nodes[j]
-    if (
-      node !== to &&
-      !node.hasPathTo(to) &&
-      rand() >= 0.5
-    ) {
-      node.pathTo(to)
-      to.pathTo(node)
-    }
-  }
-}
-const start = graph.nodes[0]
-const end = graph.nodes[graph.nodes.length - 1]
-const shortPath = findShortPathNaive(start, end)
-
-
-
 renderer.add({
   render(ctx) {
     graph.draw(ctx)
     ctx.beginPath()
     ctx.lineWidth = 7
     ctx.strokeStyle = "cyan"
-    shortPath?.forEach(p => {
-      ctx.moveTo(...p.from.position)
-      ctx.lineTo(...p.to.position)
+    ctx.moveTo(...shortPath[0].position)
+    shortPath.forEach(p => {
+      ctx.lineTo(...p.position)
+      console.log(p);
     })
     ctx.stroke()
     ctx.closePath()
-    ctx.closePath()
+    ctx.beginPath()
     ctx.moveTo(...end.position)
     circle(ctx, ...start.position, 15)
     ctx.fillStyle = "red"
@@ -162,32 +126,117 @@ renderer.add({
     ctx.closePath()
   }
 })
-renderer.pause()
+generateRandomNodes(graph, 10, renderer.width, renderer.height)
+
+const start = graph.nodes[0]
+const end = graph.nodes[graph.nodes.length - 1]
+const shortPath = findShortPath(start, end)
+
 renderer.update()
-console.log(shortPath);
+
+function generateRandomNodes(graph, number, width, height) {
+  for (let i = 0; i < number; i++) {
+    const node = new Node()
+
+    node.position.set(
+      rand(0, width),
+      rand(0, height)
+    )
+
+    graph.add(node)
+  }
+  if (graph.nodes[0]
+    .hasPathTo(graph.nodes[graph.nodes.length - 1]))
+    for (let i = 0; i < graph.nodes.length; i++) {
+      const node = graph.nodes[i]
+
+      for (let j = 0; j < graph.nodes.length; j++) {
+        const to = graph.nodes[j]
+        if (
+          node !== to &&
+          !node.hasPathTo(to) &&
+          rand() >= 0.5
+        ) {
+          node.pathTo(to)
+          to.pathTo(node)
+        }
+      }
+    }
+}
 /**
  * @param {Node} start
  * @param {Node} end
  * @returns {Path[] | null}
  */
-function findShortPathNaive(start, end,depth = 100,explored = []) {
-  if(depth < 0)return []
+function findShortPath(start, end, depth = 100, heuristic = distanceHeuristic, explored = []) {
+  if (depth < 0) return [start]
   explored.push(start)
   start.explored = true
-  let shortestpath = null
-  for (let i = 0; i < start.paths.length; i++) {
-    const path = start.paths[i]
-    if (!shortestpath && !path.to.explored)
-      shortestpath = path
-    if (shortestpath && path.distance < shortestpath.distance &&
-      !path.to.explored
-    )
-      shortestpath = path
-  }
+  const shortestpath = heuristic(start.paths, start, end)
   if (!shortestpath) return null
   const next = shortestpath.to
-  if(next === end)return [shortestpath]
-  const results = findShortPathNaive(next,end,--depth,explored)
-  if(results == void 0)return findShortPathNaive(start,end,--depth,explored)
-  return [shortestpath].concat(results)
+  if (next === end) return [start, next]
+  const results = findShortPath(next, end, --depth, heuristic, explored)
+  if (results == void 0) return findShortPath(start, end, --depth, heuristic, explored)
+  return [start].concat(results)
+}
+
+/**
+ * @param {Path[]} path
+ */
+function distanceHeuristic(paths, start, end) {
+  let shortest = null
+  let distance = Infinity
+  for (let i = 0; i < paths.length; i++) {
+    const path = paths[i]
+    const dist = new Vector2()
+      .copy(end.position)
+      .sub(path.to.position)
+      .magnitudeSquared()
+    if (!shortest && !path.to.explored) {
+      shortest = path
+      distance = dist
+    }
+    if (
+      shortest &&
+      dist < distance &&
+      !path.to.explored
+    ) {
+      shortest = path
+      distance = dist
+    }
+
+  }
+  return shortest
+}
+
+function directionHeuristic(paths, start, end) {
+  let shortest = null
+  let dotprod = -Infinity
+  for (let i = 0; i < paths.length; i++) {
+    const path = paths[i]
+    const dir = new Vector2()
+      .copy(path.to.position)
+      .sub(path.from.position)
+      .normalize()
+    const absdir = new Vector2()
+      .copy(end.position)
+      .sub(path.from.position)
+      .normalize()
+    const product = dir.dot(absdir)
+    if (!shortest && !path.to.explored) {
+      shortest = path
+      dotprod = product
+    }
+
+    if (
+      shortest && !path.to.explored &&
+      ptoduct > dotprod
+    ) {
+      shortest = path
+      dotprod = product
+    }
+  }
+  return shortest
+
 }
