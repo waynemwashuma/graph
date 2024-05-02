9918455380e12e0254b40c897e541d9fec9a1329
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-05-02 20:05:34 +0300

    Refactored the heuristics

--- src/heuristics.js
+++ src/heuristics.js
@@ -1,67 +1,31 @@
 import { Vector2 } from "../chaos.module.js"
 
 /**
- * @param {Node<Vector2>[]} paths
+ * @param {Node<Vector2>} _absstart
  * @param {Node<Vector2>} start
  * @param {Node<Vector2>} end
-*/
-export function distanceHeuristic(paths, start, end) {
-  let shortest = null
-  let distance = Infinity
-  for (let i = 0; i < paths.length; i++) {
-    const path = paths[i]
-    const dist = new Vector2()
-      .copy(end.value)
-      .sub(path.value)
-      .magnitudeSquared()
-    if (!shortest && !path.explored) {
-      shortest = path
-      distance = dist
-    }
-    if (
-      shortest &&
-      dist < distance &&
-      !path.explored
-    ) {
-      shortest = path
-      distance = dist
-    }
-
-  }
-  return shortest
+ */
+export function distanceHeuristic(start, end, _absstart) {
+  if (start === end) return -1
+  return new Vector2()
+    .copy(end.value)
+    .sub(start.value)
+    .magnitudeSquared()
 }
 /**
  * @param {Node<Vector2>[]} paths
  * @param {Node<Vector2>} start
  * @param {Node<Vector2>} end
-*/
-export function directionHeuristic(paths, start, end) {
-  let shortest = null
-  let dotprod = -Infinity
-  for (let i = 0; i < paths.length; i++) {
-    const path = paths[i]
+ */
+export function directionHeuristic(start, end, absstart) {
     const dir = new Vector2()
-      .copy(path.value)
-      .sub(start.value)
+      .copy(start.value)
+      .sub(end.value)
       .normalize()
     const absdir = new Vector2()
       .copy(end.value)
-      .sub(start.value)
+      .sub(absstart.value)
       .normalize()
-    const product = dir.dot(absdir)
-    if (!shortest && !path.explored) {
-      shortest = path
-      dotprod = product
-    }
-
-    if (
-      shortest && !path.explored &&
-      ptoduct > dotprod
-    ) {
-      shortest = path
-      dotprod = product
-    }
-  }
-  return shortest
-
+     console.log(dir.dot(absdir))
+    return dir.dot(absdir)
 }
--- src/pathfinder.js
+++ src/pathfinder.js
@@ -3,8 +3,9 @@ import { distanceHeuristic } from "./heuristics.js"
 /**
  * @param {Node<Vector2>} start
  * @param {Node<Vector2>} end
- * @param {number} [depth]
  * @param {typeof distanceHeuristic} [heuristic]
+ * @param {Node<Vector2>[]} [path]
+
  * @param {Node<Vector2>[]} [explored]
  * 
  * @returns {Node<Vector2>[] | null}
@@ -12,9 +13,24 @@ import { distanceHeuristic } from "./heuristics.js"
 export function findShortPath(start, end, heuristic = distanceHeuristic, path = [], explored = []) {
   explored.push(start)
   start.explored = true
-  const next = heuristic(start.paths, start, end)
-  path.push(start, next)
-  if (next === end) return path
+
+  let next = start.paths[0]
+  let bestcost = heuristic(next, end, start)
+  for (let i = 1; i < start.paths.length; i++) {
+    const current = start.paths[i]
+    const cost = heuristic(current, end, start)
+    if (
+      cost < bestcost &&
+      !current.explored
+    ) {
+      next = current
+      bestcost = cost
+    }
+  }
+  if (next === end) {
+    path.push(start, next)
+    return path
+  }
   findShortPath(next, end, heuristic, path, explored)
   path.unshift(start)
   return path
